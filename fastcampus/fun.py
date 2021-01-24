322acebd6c2e5f2f4d96b64f52a31dd49e3d5601
yoon-dbsrude <77610266+yoon-dbsrude@users.noreply.github.com>
2021-01-24 21:58:27 +0900

    Delete fastcampus directory

--- fastcampus/fun.py
+++
@@ -1,9 +0,0 @@
-def filter(string) :
-    if 'skip' in string :
-        print('SKIP')
-        return 
-
-user_input = ''
-while user_input != 'quit' :
-    user_input = input('Input = ')
-    filter(user_input)
